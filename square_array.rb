50b43ecaf40872e30cdb900613a3c160a3d6b22a
Priscilla Vazquez <scillamae922@gmail.com>
2018-08-12 19:07:42 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  
+  return_array = []
+  
+  array.each do |num|
+    # square my numbers
+    
+    # add the result to the return array
+  end
+  
+  return_array.push num*num
+  
 end
